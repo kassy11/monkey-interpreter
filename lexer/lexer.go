920b84b71f10c0c6c76e27127854d37a9d45aee5
kassy11 <kotaro11kassy@gmail.com>
2020-10-19 00:05:23 +0900

    finish simple lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,2 +1,60 @@
 package lexer
 
+import "github.com/kassy11/monkey-interpreter/token"
+
+type Lexer struct {
+	input string
+	position int // 現在の文字位置
+	readPosition int // 現在の文字位置の次
+	ch byte // 現在検査中の文字
+}
+
+func New(input string) *Lexer{
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// 文字を読み込んで現在位置を一つ進める
+func (l *Lexer) readChar(){
+	if l.readPosition >= len(l.input){
+		l.ch = 0
+	}else{
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token{
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token{
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
