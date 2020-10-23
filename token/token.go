8fe93e32cb29c4f32ff62349d9920593a24ba866
kassy11 <kotaro11kassy@gmail.com>
2020-10-23 15:42:39 +0900

    演算子を追加

--- token/token.go
+++ token/token.go
@@ -19,8 +19,15 @@ const (
 	INT   = "INT"
 
 	// 演算子
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// デリミタ
 	COMMA     = ","
